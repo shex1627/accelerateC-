3488dd8e54a6a5ce28640bd6f731ecdd75419424
Shicheng Huang <shichenh@ocf.berkeley.edu>
2017-01-14 08:23:25 -0800

    start learning

---
+++ grades.cpp
@@ -0,0 +1,12 @@
+#include <iomainip>
+#include <ios>
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std:cout;
+using std:setprecision;
+using std::endl;
+using std::precision;
+using std::streamsize;
+
